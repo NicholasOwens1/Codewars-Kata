232fa75179b374801d23da4c9a5b192d3270d22e
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-04-22 21:43:04 -0500

    Code under pressure

---
+++ find-odd.js
@@ -0,0 +1,14 @@
+// TODO Given an array of integers, find the one that appears an odd number of times. There will always be only one integer that appears an odd number of times.
+
+// TODO   Examples   [7] should return 7, because it occurs 1 time (which is odd).
+// TODO [0] should return 0, because it occurs 1 time (which is odd).
+// TODO [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// TODO [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// TODO [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+
+const findOdd = arr => {
+   for ()
+}
+
+console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
